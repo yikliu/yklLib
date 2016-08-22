53a6ef55096059f9133e890e486462e36c397bf7
yikliu <yikliu@me.com>
2016-08-21 22:50:53 -0500

    add leetcode

---
+++ test/leetcode/reverse_string_test.cpp
@@ -0,0 +1,24 @@
+//
+//  reverse_string_test.cpp
+//  GoogleTestExample
+//
+//  Created by yikliu on 8/21/16.
+//  Copyright © 2016 Alloysoft. All rights reserved.
+//
+
+#include <stdio.h>
+#include <gtest/gtest.h>
+#include "../../src/leetcode/344_reverse_string.h"
+
+namespace yikliu{
+    namespace test{
+        TEST(ReverseStringTest, simpleReverse)
+        {
+            std::string str("hello");
+            leetcode::ReverseString rs;
+            rs.reverseString(str);
+            std::cout<<str<<std::endl;
+            ASSERT_STREQ("olleh", str.c_str());
+        }
+    }
+}
